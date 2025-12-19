7cefb617fffef5395954f2103915f7df90a1f9cf
Sushil <sushilpawar2321@gmail.com>
2025-12-19 22:15:18 +0530

    !!

--- Mongo-Demo/index.js
+++ Mongo-Demo/index.js
@@ -8,3 +8,33 @@ mongoose
   .catch((err) => {
     console.log("Error Occuer...", err);
   });
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, lowercase : true },
+  phone: {type :Number},
+  password: { type: String, required: true },
+  hobbies : {type : [String]},
+  isVerfied : {type : Boolean, default : false}
+});
+
+const User =  mongoose.model("User", userSchema);
+async function CreatUser() {
+const userSchema  = new User({
+  name :"Sushil",
+  email :"sushil@gmail.com",
+  phone : 9034562312,
+  password :"890890",
+  hobbies : ["Coding", "Playing", "Tracking"]
+});
+
+const stroage  = await userSchema.save();
+console.log(stroage);
+}
+// CreatUser();
+
+async function getUser(params) {
+  const users = await User.find({name : "Sushil"});
+  console.log(users);
+}
+getUser();
